eff6b8a6b2709d6aa2f330c2b2357daf3aca27ea
Maxim <iangaev.m@live.fr>
2025-04-18 08:20:42 +0200

    update shop

--- wp-content/themes/victim-of-gold/woocommerce/archive-product.php
+++ wp-content/themes/victim-of-gold/woocommerce/archive-product.php
@@ -9,22 +9,15 @@ get_header('shop');
 ?>
 
 <div class="shop-container">
-    <div class="shop-header">
-        <div class="shop-header-content">
-            <h1 class="shop-title"><?php woocommerce_page_title(); ?></h1>
-            <div class="shop-filters">
-                <?php do_action('woocommerce_before_shop_loop'); ?>
-            </div>
+    <div class="shop-hero">
+        <div class="shop-hero__content">
+            <h1 class="shop-hero__title">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Boutique Graphisme.svg" alt="Boutique" class="shop-hero__title-svg">
+            </h1>
         </div>
     </div>
 
     <div class="shop-content">
-        <div class="shop-sidebar">
-            <div class="shop-sidebar-content">
-                <?php do_action('woocommerce_sidebar'); ?>
-            </div>
-        </div>
-
         <div class="shop-products">
             <?php
             if (woocommerce_product_loop()) {
