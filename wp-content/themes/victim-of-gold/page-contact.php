fc67120674ea242ee7cabd55025ccc2b502a3a5b
Maxim <iangaev.m@live.fr>
2025-07-05 01:04:51 +0200

    add contact form

---
+++ wp-content/themes/victim-of-gold/page-contact.php
@@ -0,0 +1,35 @@
+<?php
+get_header();
+?>
+<main id="main" class="site-main contact-main">
+    <section class="contact-section">
+        <div class="contact-container">
+            <div class="contact-left">
+                <div class="contact-image-bg">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact.png" alt="Contact" />
+                </div>
+                <div class="contact-infos">
+                    <div class="contact-info">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icone-email.svg" alt="Email" />
+                        <span>contact@victimofgold.com</span>
+                    </div>
+                    <div class="contact-info">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icone instagram.svg" alt="Téléphone" />
+                        <span>+33 (0)4 97 06 80 01</span>
+                    </div>
+                    <div class="contact-info">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icone-tel.svg" alt="Instagram" />
+                        <span>@vog.cannes</span>
+                    </div>
+                </div>
+            </div>
+            <div class="contact-right">
+                <h2 class="contact-title">PRENONS CONTACT</h2>
+                <div class="contact-form-wrapper">
+                    <?php echo do_shortcode('[mailpoet_form id="3"]'); ?>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<?php get_footer(); ?> 
