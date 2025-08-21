0348faaa19b52788ae735bc098e042fe4438fe87
mia <mia@ovya.fr>
2025-08-21 14:07:37 +0200

    update atelier boutique

--- wp-content/themes/victim-of-gold/woocommerce/archive-product.php
+++ wp-content/themes/victim-of-gold/woocommerce/archive-product.php
@@ -20,27 +20,46 @@ get_header('shop');
     <div class="shop-content">
         <div class="shop-products">
             <?php
-            if (woocommerce_product_loop()) {
-                woocommerce_product_loop_start();
+            // Récupérer tous les produits avec un ordre personnalisé
+            $args = [
+                'post_type' => 'product',
+                'post_status' => 'publish',
+                'posts_per_page' => -1, // Afficher tous les produits (-1 = pas de limite)
+                'orderby' => 'menu_order', // Ordre par menu_order (ordre personnalisé)
+                'order' => 'ASC', // Ordre croissant
+                'meta_query' => [
+                    [
+                        'key' => '_visibility',
+                        'value' => ['catalog', 'visible'],
+                        'compare' => 'IN'
+                    ]
+                ]
+            ];
 
-                if (wc_get_loop_prop('total')) {
-                    while (have_posts()) {
-                        the_post();
-                        do_action('woocommerce_shop_loop');
-                        wc_get_template_part('content', 'product');
-                    }
-                }
+$products_query = new WP_Query($args);
 
-                woocommerce_product_loop_end();
+if ($products_query->have_posts()) {
+    woocommerce_product_loop_start();
 
-                do_action('woocommerce_after_shop_loop');
-            } else {
-                do_action('woocommerce_no_products_found');
-            }
-            ?>
+    while ($products_query->have_posts()) {
+        $products_query->the_post();
+        do_action('woocommerce_shop_loop');
+        wc_get_template_part('content', 'product');
+    }
+
+    woocommerce_product_loop_end();
+
+    // Réinitialiser les données de post
+    wp_reset_postdata();
+
+    do_action('woocommerce_after_shop_loop');
+} else {
+    do_action('woocommerce_no_products_found');
+}
+?>
         </div>
     </div>
 </div>
 
 <?php
-get_footer('shop'); 
+get_footer('shop');
